1535b0caad1f1272b2ecd46668a236719af6a761
ceccs17d07 <44459158+ceccs17d07@users.noreply.github.com>
2018-11-08 05:31:24 +0530

    Add files via upload

---
+++ linearsearch.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main()
+
+	{
+	int A[50],i,n,x;
+	printf("Enter the number of elements: ");
+	scanf("%d",&n);
+	printf("Enter the elements:");
+	for(i=0;i<n;i++)
+		{	scanf("%d",&A[i]);
+			}
+
+	printf("Enter the element to be searched: ");
+	scanf("%d",&x);
+	for(i=0;i<n;i++)
+		{
+		{
+			if(x==A[i])
+				{	printf("%d found at %d",x,i+1);	
+					break;					
+					}
+		}
+	}
+	
+	
+
+	if(i==n)
+	{
+		printf("Not found !");
+		}
+	return 0;	
+	}
+	
+
