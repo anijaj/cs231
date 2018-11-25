3952da221fbd96e95465552257a62c3fa689043c
ceccs17d07 <44459158+ceccs17d07@users.noreply.github.com>
2018-11-25 23:06:32 +0530

    Implementation of Stack using Array

---
+++ 18_Stack_Array.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void main()
+
+{	int A[20], top=0, n, i;
+	do
+	{	printf("\nMENU\n1. PUSH\n2. POP\n3. DISPLAY\n4. EXIT\n\nEnter a Choice: ");
+		scanf("%d",&n);
+
+		if(n==1)
+		{	top=top+1;
+			printf("Enter the elements: ");
+			scanf("%d",&A[top]);
+			printf("Elements PUSHED!");
+		}
+
+		else if(n==2)
+		{	if(top==0)
+			{	printf("Stack is EMPTY!");
+			}
+
+			else
+			{	printf("Popped out %d",A[top]);
+				A[top]=NULL;
+				top-=1;
+			}
+		}
+
+		else if(n==3)
+		{	printf("Elements are: ");
+			for(i=top; i>0 ; i--)
+				{	printf("%d -> ",A[i]);
+				}
+			printf("NULL");
+		}		
+	}while(n==1||n==2||n==3);
+}
